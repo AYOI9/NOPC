d8d6a09f50b7e4e70f7275341bff7cce284bab85
maior <maior@DESKTOP-FHGLER9>
2024-12-27 09:57:22 +0200

    Добавьте файлы проекта.

---
+++ NOPC/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+class ParentClass
+{
+    public double x;
+    public double y;
+}
+
+class ChildClass : ParentClass
+{
+    public double z;
+
+    public double Calculate()
+    {
+        double maxXY = Math.Max(x, y);
+        return Math.Pow(z, 3) + maxXY;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ChildClass obj = new ChildClass();
+        obj.x = 3;
+        obj.y = 4; 
+        obj.z = 5; 
+
+        double result = obj.Calculate();
+        Console.WriteLine("Результат = " + result);
+    }
+}
